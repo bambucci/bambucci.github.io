08ec8f4123147aecf70ac9baeee0ada8317b411a
andrgotin <62072702+andrgotin@users.noreply.github.com>
2022-08-31 21:10:11 +0300

    2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 function myFunction() {
-  var x = document.getElementById("myDIV");
+  var x = document.getElementById("Vid");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
-  $("#video")[0].src += "&autoplay=1";
 }
